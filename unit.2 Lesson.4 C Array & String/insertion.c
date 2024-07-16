91598040c49574499666e3107d673c5436395f93
ahmed khaled <lvl28282@gmail.com>
2024-07-16 16:42:27 +0300

    adding ex4 insertion using array assignment

---
+++ unit.2 Lesson.4 C Array & String/insertion.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+    int n, in, loc;
+    printf("Enter no of elements: ");
+    scanf("%d", &n);
+    int elem[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("enter the element: ");
+        scanf("%d", &elem[i]);
+    }
+    
+    printf("Enter the element to be inserted: ");
+    scanf("%d", &in);
+    
+    printf("Enter the location from 1 to n: ");
+    scanf("%d", &loc);
+
+    for (int i = 1; i <= n; i++)
+    {
+        if(i == loc)
+        {
+            elem[i - 1] = in;
+        }
+        
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", elem[i]);
+        
+    }
+
+}
